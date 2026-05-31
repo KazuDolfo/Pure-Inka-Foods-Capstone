e70f3dcc6a2db8b959f245ed493928c043974e14
Kazuya <iradolfo640@gmail.com>
2026-05-31 16:32:13 -0500

    chore: update production api url

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // TODO: Replace with your actual production API URL
-  apiUrl: 'https://api.yourdomain.com/api',
+  apiUrl: 'https://pure-inka-foods-backend.onrender.com/api',
   stripePublicKey: 'pk_test_51SXsFm48OYB21cplRQsFGN933dUcmj2qoiEhTUhezUajsykWLrsPZplU1Wkul5KjD6UZrk43MYWjvGJ8cSzINgQH00n3TLIj9H'
 };
